f13cb1cf287a2c61c06d14fa976cfd245ba192c5
FUJIWARA Shunichiro <fujiwara.shunichiro@gmail.com>
2017-02-03 19:16:26 +0900

    add test

--- raus_test.go
+++ raus_test.go
@@ -1,9 +1,15 @@
 package raus_test
 
 import (
+	"os"
+	"sync"
 	"testing"
+	"time"
+
+	"golang.org/x/net/context"
 
 	"github.com/fujiwara/raus"
+	"github.com/soh335/go-test-redisserver"
 	"gopkg.in/redis.v5"
 )
 
@@ -45,6 +51,21 @@ var parseTestSet = []parseTest{
 	},
 }
 
+func TestMain(m *testing.M) {
+	conf := make(redistest.Config)
+	conf["port"] = "26379"
+	conf["save"] = ""
+	s, err := redistest.NewServer(true, conf)
+	if err != nil {
+		panic(err)
+	}
+
+	code := m.Run()
+
+	s.Stop()
+	os.Exit(code)
+}
+
 func TestParseRedisURI(t *testing.T) {
 	for _, ts := range parseTestSet {
 		opt, ns, err := raus.ParseRedisURI(ts.URI)
@@ -81,4 +102,67 @@ func TestNew(t *testing.T) {
 			t.Errorf("[min max]=%v must return error", s)
 		}
 	}
+}
+
+func TestGet(t *testing.T) {
+	ctx, cancel := context.WithCancel(context.Background())
+	r, err := raus.New("redis://localhost:26379", 0, 3)
+	if err != nil {
+		t.Error(err)
+	}
+	id, ch, err := r.Get(ctx)
+	if err != nil {
+		t.Error(err)
+	}
+	if id < 0 {
+		t.Error("cloudnot get id")
+	}
+	var wg sync.WaitGroup
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		err, more := <-ch
+		if !more {
+			return
+		}
+		t.Error(err)
+	}()
+	time.Sleep(time.Second)
+	cancel()
+	wg.Wait()
+}
+
+func TestGetRace(t *testing.T) {
+	var wg sync.WaitGroup
+	for i := 0; i <= 5; i++ {
+		wg.Add(1)
+		time.Sleep(500 * time.Millisecond)
+		go func(i int) {
+			defer wg.Done()
+			ctx, cancel := context.WithCancel(context.Background())
+			r, err := raus.New("redis://localhost:26379", 0, 5)
+			if err != nil {
+				t.Error(err)
+			}
+			id, ch, err := r.Get(ctx)
+			if err != nil {
+				t.Error(err)
+			}
+			if id < 0 {
+				t.Error("cloudnot get id")
+			}
+			wg.Add(1)
+			go func() {
+				defer wg.Done()
+				err, more := <-ch
+				if !more {
+					return
+				}
+				t.Error(err)
+			}()
+			time.Sleep(5 * time.Second)
+			cancel()
+		}(i)
+	}
+	wg.Wait()
 }
