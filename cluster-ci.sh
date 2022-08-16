b61ec451f44443e04d23e6c0de677a431cd2aec8
FUJIWARA Shunichiro <fujiwara.shunichiro@gmail.com>
2022-08-16 17:38:31 +0900

    use --exit-code-from

--- cluster-ci.sh
+++ cluster-ci.sh
@@ -2,7 +2,4 @@
 
 set -ex
 grep image: compose.yml | sort | uniq | awk '{print $2}' | xargs -I {} docker pull {}
-docker compose up -d
-sleep 10
-docker compose run app bash -c "go test -v ./..."
-docker compose down
+docker compose up --exit-code-from app
